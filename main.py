11092fff7a939bfb69e7ed3b8fd2ae4c65750c51
subinsaji <72819027+subinsaji@users.noreply.github.com>
2022-09-22 17:35:20 +0100

    all good

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 
+import random
+
 def get_choices():
 
-    player_choice = "rock"
-    computer_choice = "paper"
     
-    return computer_choice
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock",  "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
 
- 
-
-
-
-choices =  get_choices()
-print(choices)
-
-dict = {"name": "baeu", "colour": choices}
+    return choices
 
 
 
 
 
+def check_win(player, computer):
+    #print("You choise " + player + "computer chose" + computer)
+    # f string to do the same:
+    print(f"You chose {player}. Computer chose {computer}.")
+
+
+    if player == computer:
+        
+        
+        
+        
+        
+    
+
+        
+        return "Its a tie!"
 
 
 
 
-
+age = 25
+print(f"Jin is {age} years old.")
 
 
 
