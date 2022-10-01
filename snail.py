11e7d2812f7de8ec270b4bdd107e9e07c6b938dd
subinsaji <72819027+subinsaji@users.noreply.github.com>
2022-10-01 12:35:30 +0100

    snail problem

---
+++ snail.py
@@ -0,0 +1,18 @@
+
+input = [3,2,11]
+
+climbed_per_day = input[0] #climbed up per day 
+fell_per_night = input[1] #fell down per day     
+Height = input[2] #height of wall
+days = 0
+total_climbed_per_day = 0
+
+
+# time = (n-m)/H
+days = Height/(climbed_per_day-fell_per_night)
+
+
+
+
+
+
