c74bf24391bb932e3e5aa2aa61f44e57b46675dd
subinsaji <72819027+subinsaji@users.noreply.github.com>
2022-10-02 09:33:48 +0100

    outsourced solution

--- snail.py
+++ snail.py
@@ -1,4 +1,4 @@
-input = [0,0,4]
+input = [3,2,11]
 
 climbed_per_day = input[0] 
 fell_per_night = input[1]      
@@ -8,7 +8,7 @@ net_climbed_per_day = (climbed_per_day) - (fell_per_night)
 
 
 def snail_climbed():
-    if climbed_per_day == Height:
+    if climbed_per_day == Height: # if the initial height is equal to one days worth
             time = Height/climbed_per_day
             return print(int(time))
 
@@ -21,4 +21,18 @@ def snail_climbed():
 
     return print("FAIL")
             
-snail_climbed()
+#snail_climbed()
+
+
+def snail_climbing():
+    distance = climbed_per_day
+    time = 1
+    if net_climbed_per_day > 0:
+        while distance < Height:
+            distance += net_climbed_per_day
+            time += 1
+        return print(int(time))
+    else:
+        return print('Fail')
+
+snail_climbing()
