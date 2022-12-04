91348405e1289b1e07d879224ccc6bae66e350b0
subinsaji <72819027+subinsaji@users.noreply.github.com>
2022-12-04 19:04:30 +0000

    Testing Java

---
+++ Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("My name is Subin");
+        System.out.println("This feels really similar to C#");
+        System.out.println("Why didn't I start to learn all this earlier!");
+        
+        System.out.println(80);
+        System.out.println(100);
+        // This is a comment 
+        System.out.println(3+3);
+        System.out.println(3*3);
+
+
+        /*This is a multi line comment/ comment block
+         * NICE
+         */
+
+
+        // Below is the Java Variable Types
+
+        /* String - stores text such as "Hello"
+         * int - stores integers (whole numbers)
+         * float - stores floating point numbers i.e. decimals
+         * char - single store characters such as "a" or "B"
+         * boolean - stores two states: true or false
+         */
+        
+         String name = "Subin";
+         System.out.println(name);
+
+         final int myNum = 15;
+
+         System.out.println(myNum);
+
+         String carName = "Volvo"
+         
+
+    }
+}
